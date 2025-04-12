0d87c75459d8cf03e68b0f5395d52fad15107b7a
SauravBudhathoki <budhathokisaurav354@gmail.com>
2025-04-12 22:13:33 +0545

    init commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
+<<<<<<< HEAD
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
 }
+=======
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String[] questions={"What is the main function of a router?",
+                            "Which part of the computer is considered as the brain of the computer?",
+                            "What year wa Facebook launched?",
+                            "Who is known as the father of computer?",
+                            "What was the first programming language?"};
+
+        String[][] options={{"1. Storing messages.","2. Directing internet Traffic.","3. Managing password."},
+                            {"1. Keyboard.","2. CPU","3. GPU"},
+                            {"1. 2000","2. 2001","3. 2004"},
+                            {"1. Charles Babbage","2. Albert Einstien","3. Mary Curie"},
+                            {"1. Fortan","2. C++","3. COBOL"}};
+
+        int[] answers={2,2,3,1,1};
+        int score;
+        int guess;
+
+        Scanner scanner =new Scanner(System.in);
+
+
+
+
+
+
+
+
+
+
+
+        scanner.close();
+    }
+}
+>>>>>>> 0915868 (init commit)
